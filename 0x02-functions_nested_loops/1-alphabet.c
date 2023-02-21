1b89ea84295afcd2bb8cc4e82375db134b1afea7
Hezekiah-OSuolale <osuoalehezekiah75@gmail.com>
2023-02-21 06:50:29 -0800

    adding main.h

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * print_alphabet
+ *
+ * returns (0);
+ */
+
+void print_alphabet(void)
+{
+	char letter = "a";
+
+	while (letter <= "z")
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
+	
